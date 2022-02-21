77519ebab9412b9056bf09b733d76ce2ce91e824
rfrias8 <rfrias8@gmail.com>
2022-02-21 13:59:16 -0600

    added js file

---
+++ main.js
@@ -0,0 +1,10 @@
+function getFortune() {
+    // get the button
+    let myFortune = document.getElementById('');
+
+    // get the current value of the button's display property
+    let displaySetting = myButton.style.display;
+    
+    //get the button so that we can change it
+    const fortuneButton = document.getElementById('button');
+}
