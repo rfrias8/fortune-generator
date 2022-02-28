7b9d4fa8b9ade98604ba61278f4140b1696a52f0
rfrias8 <rfrias8@gmail.com>
2022-02-27 19:06:12 -0600

    update fortunes

--- main.js
+++ main.js
@@ -9,42 +9,42 @@ fortuneButton.onclick = function getFortune() {
     let number = getRandomInt(10);
         switch(number){
         case 1:
-            document.getElementById('output').innerHTML = "You are awesome";
+            document.getElementById('output').innerHTML = "Love is in the air";
             break;
         case 2:
-            document.getElementById('output').innerHTML = "Hello";
+            document.getElementById('output').innerHTML = "What you have bee wanting for so long will soon come true";
             break;
         case 3:
-            document.getElementById('output').innerHTML = "You rock";
+            document.getElementById('output').innerHTML = "A beautiful, smart, and loving person will be coming into your life";
             break;
         case 4:
-            document.getElementById('output').innerHTML = "You stink";
+            document.getElementById('output').innerHTML = "All your hard work will soon pay off";
             break;
         case 5:
-            document.getElementById('output').innerHTML = "You da best";
+            document.getElementById('output').innerHTML = "Everywhere you choose to go, friendly faces will greet you";
             break;
         case 6:
-            document.getElementById('output').innerHTML = "Salsa";
+            document.getElementById('output').innerHTML = "Failure is the chance to do better next time";
             break;
         case 7:
-            document.getElementById('output').innerHTML = "hungry";
+            document.getElementById('output').innerHTML = "Go for the gold today! You will be the champion of whatever";
             break;
         case 8:
-            document.getElementById('output').innerHTML = "pizza";
+            document.getElementById('output').innerHTML = "If you continually give, you will continually have";
             break;
         case 9:
-            document.getElementById('output').innerHTML = "phone";
+            document.getElementById('output').innerHTML = "It is better to deal with problems before they arise";
             break;
         case 10:
-            document.getElementById('output').innerHTML = "vacation";
+            document.getElementById('output').innerHTML = "New people will bring you new realizations, especially about big issues";
             break;
         default:
-            document.getElementById('output').innerHTML = "The stars are unclear, come back for your fortune tomorrow.";
+            document.getElementById('output').innerHTML = "Fortune Not Found: Abort, Retry, Ignore?";
        }
 };
 
 fortuneButton.onmouseover = function() {
-    document.getElementById('button').style.backgroundColor = "blue";
+    document.getElementById('button').style.backgroundColor = "#839baf";
 };
 
 fortuneButton.onmouseout = function() {
